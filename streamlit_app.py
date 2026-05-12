baf459d2350d6aaf9b54dbbbd3d33097864705bc
lpkkelompok5 <lpkkelompok5@gmail.com>
2026-05-12 09:10:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("kalkulator pintar")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.title("Kalkulator rawrrrr")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
